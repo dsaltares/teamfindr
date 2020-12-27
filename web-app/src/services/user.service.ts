5ac4d94ba7b328feac672c74b48e93eef2fcc14a
David Saltares <david.saltares@gmail.com>
2020-12-27 10:22:20 +0200

    handle 401 from verify

--- web-app/src/services/user.service.ts
+++ web-app/src/services/user.service.ts
@@ -3,18 +3,25 @@ import { User, AuthProvider } from '../types';
 import { API_URL } from '../endpoints';
 
 const userService = {
-  verify: async (): Promise<User> => {
-    const {
-      data: { user },
-    } = await axios.get(`${API_URL}/auth/verify`, {
-      withCredentials: true,
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Credentials': true,
-      },
-    });
-    return user;
+  verify: async (): Promise<User | null> => {
+    try {
+      const {
+        data: { user },
+      } = await axios.get(`${API_URL}/auth/verify`, {
+        withCredentials: true,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Credentials': true,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error?.response?.status === 401) {
+        return null;
+      }
+      throw error;
+    }
   },
   openIdpAuthPage: (provider: AuthProvider) => {
     window.open(`${API_URL}/auth/${provider}`, '_self');
