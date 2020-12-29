89630405b0525baeb9f4c93f32c58ab31dfe2e03
David Saltares <david.saltares@gmail.com>
2020-12-29 19:56:15 +0200

    point web-app to prod endpoints in prod

--- web-app/src/endpoints.ts
+++ web-app/src/endpoints.ts
@@ -1,4 +1,4 @@
 export const API_URL =
-  process.env.NODE_ENV === 'development' || true
+  process.env.NODE_ENV === 'development'
     ? 'http://localhost:5000'
     : 'https://api.teamfindr.saltares.com';
