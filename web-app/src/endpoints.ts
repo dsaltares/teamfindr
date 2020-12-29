f3fcb286b412137ab60414b836c6077a391dee0e
David Saltares <david.saltares@gmail.com>
2020-12-29 10:47:22 +0200

    make webapp talk to local server even in prod just to test dev data on my device

--- web-app/src/endpoints.ts
+++ web-app/src/endpoints.ts
@@ -1,4 +1,4 @@
 export const API_URL =
-  process.env.NODE_ENV === 'development'
+  process.env.NODE_ENV === 'development' || true
     ? 'http://localhost:5000'
     : 'https://api.teamfindr.saltares.com';
