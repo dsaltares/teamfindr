c2f4edc79aedd8fc9d44dc00373020c01c58116b
David Saltares <david.saltares@gmail.com>
2021-01-03 17:58:06 +0200

    open google maps with directions

--- web-app/src/utils/getGoogleMapsUrl.ts
+++ web-app/src/utils/getGoogleMapsUrl.ts
@@ -1,4 +1,3 @@
-import { isIOS } from 'react-device-detect';
 import { Venue } from '../types';
 
 const getGoogleMapsUrl = (venue?: Venue) => {
@@ -7,8 +6,7 @@ const getGoogleMapsUrl = (venue?: Venue) => {
   }
 
   const [lon, lat] = venue.location.geo.coordinates;
-  const protocol = isIOS ? `maps` : `https`;
-  return `${protocol}://maps.google.com/maps?daddr=${lat},${lon}&amp;ll=`;
+  return `https://www.google.com/maps/dir/?api=1&destination=${lat},${lon}`;
 };
 
 export default getGoogleMapsUrl;
