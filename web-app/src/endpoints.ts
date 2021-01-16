f5b3833ea7111d2d4ae3f71e5b4494dfd680f64e
David Saltares <david.saltares@gmail.com>
2021-01-16 18:56:59 +0200

    basic service worker

--- web-app/src/endpoints.ts
+++ web-app/src/endpoints.ts
@@ -1,4 +1,5 @@
 export const API_URL =
-  process.env.NODE_ENV === 'development'
+  process.env.NODE_ENV === 'development' ||
+  window.location.hostname === 'localhost'
     ? 'http://localhost:5000'
     : 'https://api.teamfindr.saltares.com';
--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import Providers from './providers';
 
@@ -17,6 +18,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
