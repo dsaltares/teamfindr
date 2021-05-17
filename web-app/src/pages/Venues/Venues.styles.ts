fae2ae54d86fe8a247454dc8372ef0ff09a6da17
David Saltares <david.saltares@gmail.com>
2021-05-17 09:33:48 +0300

    venues basic styling

--- web-app/src/pages/Venues/Venues.styles.ts
+++ web-app/src/pages/Venues/Venues.styles.ts
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   filtersPaper: {
     padding: theme.spacing(2),
+    boxShadow: '0px 4px 21px rgba(32, 32, 32, 0.15)',
+    borderRadius: 5,
   },
   venuesPaper: {
     minHeight: '100%',
