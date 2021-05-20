7ae15e3cd6da21903d19dd1ea235fe30e5e73b4a
David Saltares <david.saltares@gmail.com>
2021-05-20 20:19:04 +0300

    venue page styling

--- web-app/src/components/VenueBasicInfoPanel/VenueBasicInfoPanel.styles.ts
+++ web-app/src/components/VenueBasicInfoPanel/VenueBasicInfoPanel.styles.ts
@@ -1,11 +1,16 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
-  card: {
-    width: '100%',
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    boxShadow: '0px 4px 21px rgba(32, 32, 32, 0.15)',
+    borderRadius: 5,
   },
-  address: {
-    flex: 1,
+  venueImage: {
+    width: '100%',
+    height: 125,
+    objectFit: 'cover',
+    borderRadius: 6,
   },
 }));
 
--- web-app/src/components/VenueBasicInfoPanel/VenueBasicInfoPanel.tsx
+++ web-app/src/components/VenueBasicInfoPanel/VenueBasicInfoPanel.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
@@ -20,39 +17,57 @@ const VenueBasicInfoPanel: React.FC<VenueBasicInfoPanelProps> = ({ venue }) => {
   const classes = useStyles();
 
   return (
-    <Card className={classes.card}>
-      <CardActionArea
-        component={Link}
-        href={getGoogleMapsUrl(venue)}
-        rel="nofollow noopener"
-        target="_blank"
-        color="inherit"
-        underline="none"
-        disabled={!venue}
-      >
-        <CardContent>
-          <Grid container direction="row" spacing={2} alignItems="center">
-            <Grid item>
-              <Typography color="textSecondary">
-                <LocationOnIcon />
-              </Typography>
+    <Paper className={classes.paper}>
+      <Grid container direction="column" spacing={2}>
+        <Grid item>
+          <img
+            className={classes.venueImage}
+            src="https://scontent.fclj2-1.fna.fbcdn.net/v/t1.6435-9/106371406_3091783634236528_3551108379228035343_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=e3f864&_nc_ohc=zuBxzdF-jzkAX_IWMsH&_nc_ht=scontent.fclj2-1.fna&oh=6c8a403c02b5c2366512e653d3a0dd07&oe=60BC1AD0"
+            alt="venue"
+          />
+        </Grid>
+        <Grid item>
+          <Link
+            href={getGoogleMapsUrl(venue)}
+            rel="nofollow noopener"
+            target="_blank"
+            color="inherit"
+            underline="none"
+          >
+            <Grid container direction="row" spacing={1} alignItems="center">
+              <Grid item>
+                <LocationOnIcon color="primary" />
+              </Grid>
+              <Grid item>
+                <Grid container direction="column">
+                  <Grid item>
+                    {venue ? (
+                      <Typography variant="body1" color="textPrimary">
+                        {venue.name}
+                      </Typography>
+                    ) : (
+                      <Skeleton variant="text" />
+                    )}
+                  </Grid>
+                  <Grid item>
+                    {venue ? (
+                      <Typography variant="body2" color="textSecondary">
+                        {venue.location.description}
+                      </Typography>
+                    ) : (
+                      <Skeleton variant="text" />
+                    )}
+                  </Grid>
+                </Grid>
+              </Grid>
             </Grid>
-            <Grid item className={classes.address}>
-              {venue ? (
-                <Typography color="textSecondary">
-                  {venue.location.description || venue.location.name}
-                </Typography>
-              ) : (
-                <Skeleton variant="text" />
-              )}
-            </Grid>
-          </Grid>
-        </CardContent>
-      </CardActionArea>
-      <CardMedia>
-        <Map location={venue?.location || null} />
-      </CardMedia>
-    </Card>
+          </Link>
+        </Grid>
+        <Grid item>
+          <Map location={venue?.location || null} />
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
 
