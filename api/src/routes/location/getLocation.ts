597fc34162e405ddfe3d788dcc23f9c3fb97fd4a
David Saltares <david.saltares@gmail.com>
2021-06-03 14:52:36 +0300

    fix getLocation

--- api/src/routes/location/getLocation.ts
+++ api/src/routes/location/getLocation.ts
@@ -6,7 +6,7 @@ const GetLocationController: ControllerCreator = () => async ({
   connection,
 }) => {
   const ip = headers['x-forwarded-for'] || connection.remoteAddress;
-  const geo = geoip.lookup(ip as string);
+  const geo = geoip.lookup((ip as string).split(',')[1]);
 
   if (!geo) {
     return {
