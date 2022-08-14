b1934fed1a4cd8d0a94bd92e6f38a8e362b5e0cc
David Saltares <david.saltares@gmail.com>
2022-08-14 15:04:44 +0300

    fix: storybook in nextjs

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -1,11 +1,15 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
+    return config;
+  },
+};
--- app/.storybook/preview.js
+++ app/.storybook/preview.js
@@ -2,7 +2,7 @@ import '@fontsource/poppins';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 import { HashRouter } from 'react-router-dom';
-import ThemeProvider from '../src/providers/ThemeProvider';
+import ThemeProvider from '@components/providers/ThemeProvider';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
